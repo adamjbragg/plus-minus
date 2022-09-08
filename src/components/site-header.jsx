3aa0b43f4eef8b72b3a618ca39f76fb8ecdcf194
Adam J Bragg <mail@adamjbragg.net>
2022-09-09 09:37:51 +1000

    style, add logo, time, placeholder text

--- src/components/site-header.jsx
+++ src/components/site-header.jsx
@@ -1,16 +1,23 @@
 import Link from 'next/link';
-import HeaderNavList from './header-navigation';
 import general from '@/data/general.json';
+import TimeDateLocation from './time-date-location';
+import Logo from './logo';
 
 const Header = function () {
 	return (
-		<header className="w-full">
+		<header className="w-full py-5">
 			<div className="px-5 mx-auto">
-				<div className="flex items-center justify-between py-5 border-b">
+				<div className="flex w-full flex-row justify-center items-center py-5">
+					<h1 className="sr-only">{general.siteTitle}</h1>
 					<Link href="/">
-						<a aria-label="Home Logo">{general.siteTitle}</a>
+						<a className="fixed left-5">
+							<Logo />
+						</a>
 					</Link>
-					<HeaderNavList />
+					<div className="text-center">
+						<TimeDateLocation />
+					</div>
+					<p className="fixed right-5">Start a Project</p>
 				</div>
 			</div>
 		</header>
