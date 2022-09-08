44ec98d9583ac0bf582efe7fb2769d58f49e3236
Adam J Bragg <mail@adamjbragg.net>
2022-09-09 09:37:11 +1000

    update title

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,8 +1,8 @@
 export default {
-  openGraph: {
-    type: 'website',
-    locale: 'en_AU',
-    url: process.env.NEXT_PUBLIC_SITE_URL,
-    site_name: 'AJB Next Starter',
-  },
+	openGraph: {
+		type: 'website',
+		locale: 'en_AU',
+		url: process.env.NEXT_PUBLIC_SITE_URL,
+		site_name: 'Plus Minus',
+	},
 };
