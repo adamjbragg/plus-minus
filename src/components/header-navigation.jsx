4f4627395159a8dddf92c76c7d876e19684a4087
Adam J Bragg <mail@adamjbragg.net>
2022-09-09 09:37:21 +1000

    style nav

--- src/components/header-navigation.jsx
+++ src/components/header-navigation.jsx
@@ -7,23 +7,25 @@ const HeaderNavList = function () {
 	const router = useRouter();
 
 	return (
-		<ul className="flex text-justify space-x-5">
-			{nav.items.map((item) => (
-				<li key={item.title}>
-					<Link href={item.url}>
-						<a
-							className={`${
-								router.pathname === item.url
-									? 'text-red-500 '
-									: 'hover:opacity-50'
-							} border-transparent `}
-						>
-							{item.title}
-						</a>
-					</Link>
-				</li>
-			))}
-		</ul>
+		<div className="flex fixed bottom-16 w-full mx-auto left-1/2 -translate-x-1/2 justify-center px-8 z-10">
+			<ul className="flex justify-around items-center p-4 space-x-3 rounded-full w-fit shadow-lg z-10 bg-white">
+				{nav.items.map((item) => (
+					<li key={item.title}>
+						<Link href={item.url}>
+							<a
+								className={`${
+									router.pathname === item.url
+										? 'text-red-500 '
+										: 'text-black hover:opacity-50'
+								}  border-transparent `}
+							>
+								{item.title}
+							</a>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
 	);
 };
 
