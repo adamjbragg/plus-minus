95bfa00584ed9b951d1ad60b5e8db89b96471830
Adam J Bragg <mail@adamjbragg.net>
2022-09-09 09:37:26 +1000

    add logo svg

---
+++ src/components/logo.jsx
@@ -0,0 +1,15 @@
+export default function Logo() {
+	return (
+		<svg
+			width="35"
+			height="50"
+			viewBox="0 0 35 50"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path d="M0 49.9277V43.9277H35V49.9277H0Z" fill="white" />
+			<path d="M20.0723 0H13.9999L13.9999 35H20.0723L20.0723 0Z" fill="white" />
+			<path d="M35 20V13.9277L5.3086e-07 13.9277L0 20L35 20Z" fill="white" />
+		</svg>
+	);
+}
