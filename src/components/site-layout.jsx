5c1ec03cded2937e0522187de56405bdf8e42786
Adam J Bragg <mail@adamjbragg.net>
2022-09-08 16:43:28 +1000

    playing with gradient animations

--- src/components/site-layout.jsx
+++ src/components/site-layout.jsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import { NextSeo } from 'next-seo';
+import { motion } from 'framer-motion';
 import Header from './site-header';
 import Footer from './site-footer';
 
@@ -12,7 +13,26 @@ const Layout = function ({
 	const canonicalURL = `${process.env.NEXT_PUBLIC_SITE_URL}${router.pathname}`;
 
 	return (
-		<div className="flex flex-col items-center justify-center min-h-full">
+		<motion.div
+			layout
+			animate={{
+				backgroundImage: `radial-gradient(at 50% 50%, hsla(240, 97%, 13%, 1) 0px, transparent 50%),radial-gradient(at 25% 25%, hsla(240, 97%, 13%, 1) 0px, transparent 50%)`,
+				backgroundColor: [
+					'hsla(135, 0%, 0%, 1)',
+					'hsla(236, 100%, 8%, 1)',
+					'hsla(248, 97%, 14%, 1)',
+					'hsla(135, 0%, 0%, 1)',
+				],
+			}}
+			transition={{
+				duration: 2,
+				ease: 'easeInOut',
+				times: [0, 0.2, 0.5, 0.8, 1],
+				repeat: Infinity,
+				repeatDelay: 1,
+			}}
+			className="flex flex-col items-center justify-center min-h-full"
+		>
 			<NextSeo
 				title={pageTitle}
 				titleTemplate="AJB Next Starter | %s"
@@ -24,7 +44,7 @@ const Layout = function ({
 				{children}
 			</main>
 			<Footer />
-		</div>
+		</motion.div>
 	);
 };
 
