740ccbbf749a9c9c8ee3171d2ab7ab6db46803ca
Adam J Bragg <mail@adamjbragg.net>
2022-09-09 11:03:25 +1000

    show after load

--- src/components/time-date-location.jsx
+++ src/components/time-date-location.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useInView } from 'framer-motion';
+import { useState, useEffect, useRef } from 'react';
 
 // set the date to a new current date for load
 // create an effect that runs on a 1 second interval to update the date
@@ -6,8 +7,10 @@ import { useState, useEffect } from 'react';
 // location is hardcoded
 
 export default function TimeDateLocation() {
-	const [date, setDate] = useState(new Date());
+	const ref = useRef(null);
+	const isInView = useInView(ref, { once: true });
 
+	const [date, setDate] = useState(new Date());
 	// options for formatting the time and date
 	const timeOptions = {
 		hour: 'numeric',
@@ -28,7 +31,14 @@ export default function TimeDateLocation() {
 	}, []);
 
 	return (
-		<div className="text-xs">
+		<div
+			className="text-xs"
+			ref={ref}
+			style={{
+				opacity: isInView ? 1 : 0,
+				transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 1.5s',
+			}}
+		>
 			<span>{date.toLocaleString('en-AU', timeOptions)}</span>
 			<span> • {date.toLocaleString('en-AU', dateOptions)}</span>
 			<span> • Canberra, Australia</span>
