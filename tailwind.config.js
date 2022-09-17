65baaf3b8114b67e04a69a31a39e831656d68990
Adam J Bragg <mail@adamjbragg.net>
2022-09-17 15:14:00 +1000

    continue work and setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 module.exports = {
 	content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			'space-sans': ['Space Grotesk', 'sans-serif'],
+		},
 		extend: {},
 	},
 	variants: {
 		extend: {},
 	},
-	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/line-clamp'),
+	],
 };
