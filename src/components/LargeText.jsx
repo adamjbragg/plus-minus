01816ce8c7fccc21b3ed4efd3800df84fd76f94a
Adam J Bragg <mail@adamjbragg.net>
2022-09-22 15:38:20 +1000

    create split word function

--- src/components/LargeText.jsx
+++ src/components/LargeText.jsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion';
+import { classNames } from 'utilities/classNames';
 
 const listVariants = {
 	animate: {
@@ -12,7 +13,7 @@ const listVariants = {
 const itemVariants = {
 	initial: { y: 600 },
 	animate: {
-		y: 0,
+		y: -50,
 		transition: {
 			ease: [0.6, 0.01, -0.05, 0.95],
 			duration: 1,
@@ -27,21 +28,39 @@ const uid = () =>
 		''
 	);
 
-export default function LargeText({ text }) {
+export default function LargeText({ text, className }) {
+	const splitWords = (words) => {
+		const textSpread = [...words];
+
+		if (textSpread.includes(' ')) {
+			const indexToSplit = textSpread.indexOf(' ');
+			const first = textSpread.slice(0, indexToSplit);
+			const second = textSpread.slice(indexToSplit + 1);
+
+			const split = [first, second];
+
+			return split;
+		}
+
+		return [...words];
+	};
+
+	console.log(splitWords(text));
 	return (
 		<motion.div
 			variants={listVariants}
 			initial="initial"
 			whileInView="animate" // apply the animate variant when the div is in view
 			viewport={{ once: true, amount: 0.5 }}
-			className="flex flex-row text-[32vw] absolute overflow-hidden -left-36 text-pm-purple mix-blend-multiply opacity-40 font-bold leading-[1.25] -top-60 -z-10"
+			className={classNames(
+				'flex absolute overflow-hidden mix-blend-multiply opacity-100',
+				'text-[27vw] text-pm-purple font-bold leading-none',
+				'-translate-x-32 -translate-y-64 -z-10',
+				className || null
+			)}
 		>
-			{[...text].map((letter) => (
-				<motion.span
-					className="flex-1"
-					variants={itemVariants}
-					key={`item-${letter}-${uid()}`}
-				>
+			{splitWords(text).map((letter) => (
+				<motion.span variants={itemVariants} key={`item-${letter}-${uid()}`}>
 					{letter}
 				</motion.span>
 			))}
