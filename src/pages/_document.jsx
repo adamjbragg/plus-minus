0ea53f67195470fa0411b1bf1a08919ba8b5afd1
Adam J Bragg <mail@adamjbragg.net>
2022-09-26 11:26:27 +1000

    change font display to optional

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
 					<link
-						href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+						href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=optional"
 						rel="stylesheet"
 					/>
 					<script
