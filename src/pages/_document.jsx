6b3ba6da34315cbe3dbd93b5bc7b581349ad094e
Adam J Bragg <mail@adamjbragg.net>
2022-09-26 11:24:24 +1000

    remove background gradients

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -34,8 +34,6 @@ class MyDocument extends Document {
 						}}
 					/>
 				</Head>
-				<div className="linear absolute h-full w-full opacity-100 overflow-hidden -z-20" />
-				<div className="radial absolute h-full w-full opacity-100 overflow-hidden mix-blend-overlay -z-30" />
 				<body>
 					<Main />
 					<NextScript />
