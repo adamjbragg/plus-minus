e5c82a09bb4739d085b0d5d7a29563b55c6cae5f
Adam J Bragg <mail@adamjbragg.net>
2022-10-13 14:19:41 +1100

    self host fonts

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,7 +2,7 @@ import LargeText from '@/components/LargeText';
 
 export default function Section({ largeText, children }) {
 	return (
-		<section className="relative flex flex-col items-start h-[70vh]">
+		<section className="relative flex flex-col items-start h-screen">
 			{largeText && <LargeText text={largeText} />}
 			<div className="pt-36 space-y-12">{children}</div>
 		</section>
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -11,16 +11,16 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head>
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					{/* <link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
 					<link
-						href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=optional"
+						href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
 						rel="stylesheet"
 					/>
 					<script
 						async
 						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-					/>
+					/> */}
 					<script
 						dangerouslySetInnerHTML={{
 							__html: `
